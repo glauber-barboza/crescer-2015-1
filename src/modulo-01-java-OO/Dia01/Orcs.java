f2f1211fba6505f4fcff434776eeb04fce80c842
Glauber cardoso barboza <glauber.barboza@hotmail.com>
2015-04-06 14:17:54 -0300

    Adicionado testes atacks no orc

--- src/modulo-01-java-OO/Dia01/Orcs.java
+++ src/modulo-01-java-OO/Dia01/Orcs.java
@@ -21,13 +21,6 @@ public class Orcs
         nome = "Bazinga";
         hp = 110;
     }
-
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
    //orc recebe 10 de dano
     public int recebeDano(){
      hp = hp-10;   
--- src/modulo-01-java-OO/Dia01/OrcsTest.java
+++ src/modulo-01-java-OO/Dia01/OrcsTest.java
@@ -18,7 +18,8 @@ public class OrcsTest
      */
     public OrcsTest()
     {   
-        @Test
+       }
+            @Test
         public void orcNascecomVida(){
             //Arrange - Montagem dos dados de testes
             Orcs umOrc=new Orcs();
@@ -31,6 +32,50 @@ public class OrcsTest
             
             
     }
-
-       }
+         @Test
+        public void orcRecebeUmAtack(){
+            //Arrange - Montagem dos dados de testes
+            Orcs umOrc=new Orcs();
+            int esperado=100;
+            int resultadoObtido = umOrc.recebeDano();
+            
+            assertEquals(resultadoObtido, esperado);
+            //Act - Execução da ação de testes
+            //Assert - Verifica se o resultado é o esperado
+            
+            
+    }
+    
+    //teste no orck ele recebe 2 atack
+        @Test
+        public void orcRecebeDoisAtack(){
+            //Arrange - Montagem dos dados de testes
+            Orcs umOrc=new Orcs();
+            int esperado=90;
+            int resultadoObtido;
+            resultadoObtido= umOrc.recebeDano();
+            resultadoObtido= umOrc.recebeDano();
+            assertEquals(resultadoObtido, esperado);
+            //Act - Execução da ação de testes
+            //Assert - Verifica se o resultado é o esperado
+            
+    }
+    //teste no orck ele recebe 5 atack
+        @Test
+        public void orcRecebeCincoAtack(){
+            //Arrange - Montagem dos dados de testes
+            Orcs umOrc=new Orcs();
+            int esperado=60;
+            int resultadoObtido;
+            resultadoObtido= umOrc.recebeDano();
+            resultadoObtido= umOrc.recebeDano();
+            resultadoObtido= umOrc.recebeDano();
+            resultadoObtido= umOrc.recebeDano();
+            resultadoObtido= umOrc.recebeDano();
+            assertEquals(resultadoObtido, esperado);
+            //Act - Execução da ação de testes
+            //Assert - Verifica se o resultado é o esperado
+            
+            
+    }
     }
