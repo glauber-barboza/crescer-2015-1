46f52ebf0670cb4cc662e7aa3f1ea4bc0331ff6c
Glauber cardoso barboza <glauber.barboza@hotmail.com>
2015-04-08 15:31:19 -0300

    Contador de numero serve para explicar o for

---
+++ src/modulo-01-java-OO/Dia01/ContadorDeNumeros.java
@@ -0,0 +1,26 @@
+
+/**
+ * Write a description of class ContadorDeNumeros here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class ContadorDeNumeros
+{
+  
+    /**
+     * Constructor for objects of class ContadorDeNumeros
+     */
+    public ContadorDeNumeros()
+    {
+    }
+    public void contarAte(int numero){
+        
+        
+        for(int i=1; i<=numero; i++){
+        System.out.println(i);
+    }
+    
+    }
+    }
+
