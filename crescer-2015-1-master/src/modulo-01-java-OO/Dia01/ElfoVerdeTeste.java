ca8c629e3109a607a796f1a71e5f90df1a8b5e86
Glauber cardoso barboza <glauber.barboza@hotmail.com>
2015-04-13 15:17:28 -0300

    Testes(Elfos)= elfosNoturnos atacam 1 vez, elfosNoturnos atacam 2 vez, elfosNoturnos atacam 10 vezes.

--- crescer-2015-1-master/src/modulo-01-java-OO/Dia01/ElfoVerdeTeste.java
+++ crescer-2015-1-master/src/modulo-01-java-OO/Dia01/ElfoVerdeTeste.java
@@ -100,4 +100,59 @@ public class ElfoVerdeTeste
         ArrayList<ItemDoInventario> inventarioObtido = elfoVerde.getItens();
         assertEquals(inventarioEsperado, inventarioObtido);
     }
+    
+     @Test
+    public void ElfoNoturnoAtacaUmaVez() {
+        Elfo elfoNoturno=new Elfo("Tio Patinhas");
+        Orc shureck=new Orc();
+        
+        elfoNoturno.elfoNorutnoAtaca(shureck);
+        
+        int vidaEsperada= elfoNoturno.getVida();
+        int experienciaExperada= elfoNoturno.getExperiencia();
+       
+        assertEquals(95, vidaEsperada);
+        
+        assertEquals(3, experienciaExperada);
+    }
+    
+     @Test
+    public void ElfoNoturnoAtacaDuasVezes() {
+        Elfo elfoNoturno=new Elfo("Tio Patinhas");
+        Orc shureck=new Orc();
+        
+        elfoNoturno.elfoNorutnoAtaca(shureck);
+        elfoNoturno.elfoNorutnoAtaca(shureck);
+        
+        int vidaEsperada= elfoNoturno.getVida();
+        int experienciaExperada= elfoNoturno.getExperiencia();
+       
+        assertEquals(90, vidaEsperada);
+        
+        assertEquals(6, experienciaExperada);
+    }
+    
+     @Test
+    public void ElfoNoturnoAtacaDezVezes() {
+        Elfo elfoNoturno=new Elfo("Tio Patinhas");
+        Orc shureck=new Orc();
+        
+        elfoNoturno.elfoNorutnoAtaca(shureck);
+        elfoNoturno.elfoNorutnoAtaca(shureck);
+        elfoNoturno.elfoNorutnoAtaca(shureck);
+        elfoNoturno.elfoNorutnoAtaca(shureck);
+        elfoNoturno.elfoNorutnoAtaca(shureck);
+        elfoNoturno.elfoNorutnoAtaca(shureck);
+        elfoNoturno.elfoNorutnoAtaca(shureck);
+        elfoNoturno.elfoNorutnoAtaca(shureck);
+        elfoNoturno.elfoNorutnoAtaca(shureck);
+        elfoNoturno.elfoNorutnoAtaca(shureck);
+        
+        int vidaEsperada= elfoNoturno.getVida();
+        int experienciaExperada= elfoNoturno.getExperiencia();
+       
+        assertEquals(57, vidaEsperada);
+        
+        assertEquals(30, experienciaExperada);
+    }
 }
