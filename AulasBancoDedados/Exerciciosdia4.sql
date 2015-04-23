17d1993798dbd09ed1327bc3aa0784b9a2c1db5b
Glauber cardoso barboza <glauber.barboza@hotmail.com>
2015-04-23 14:16:12 -0300

    Correção dos exercicios

--- AulasBancoDedados/Exerciciosdia4.sql
+++ AulasBancoDedados/Exerciciosdia4.sql
@@ -72,21 +72,73 @@ having count(1) > 1
 insert into CidadeAux(IDCidade, Nome, UF) 
 select IDCidade, Nome, UF from Cidade;
 
---11 nao pronto
+
 --11)Altere todas cidades duplicadas (nome e uf iguais), acrescente no ínicio do nome um asterisco (*).
-select  Nome
+
+begin transaction
+go
+update Cidade
+set Nome= '*'+ nome
+where nome in (select  Nome
 from Cidade
 group by Nome
 having count(1) > 1
-
+)
+	commit
 
 -- 12)Faça uma consulta que liste o nome do Associado e a descrição da coluna Sexo,
 -- informando: Masculino ou Feminino.
 
 select IDAssociado,
 Nome,
-Case when sexo = 'F' then 'Feminino'
-when sexo = 'M' then 'Masculino'
-else 'sei lá'
-End Genero
+	Case when sexo = 'F' then 'Feminino'
+	  when sexo = 'M' then 'Masculino'
+	  else 'sei lá'
+	End Genero
 from associado;
+
+--13)Faça uma consulta que mostre o nome do empregado, 
+--o Salario e percentual a ser descontado do Imposto de Renda, considerando a tabela abaixo:
+--Até R$ 1.164,00 = 0%De R$ 1.164,00 a R$ 2.326,00 = 15%
+--Acima de R$ 2.326,00 = 27,5%.
+
+select NomeEmpregado,
+	   Salario,
+Case 
+	when Salario <= 1160 then 0
+	when Salario > 1164 and Salario <= 2326 then (salario*0.15)
+	when Salario > 2326 then (salario*0.275)
+	else 0
+	End Desconto_IR
+from Empregado
+
+--14)Elimine as cidades duplicadas (mantendo 1 registro para cada).
+ 
+ begin transaction
+ go
+
+Delete from Cidade
+where IDCidade in (Select MAX(IDCidade)
+					from Cidade
+					group by Nome, uf
+					having count(1)>1)
+
+
+Select max(IdCidade) maior_id_cidade_duplicada
+from cidade  
+group by nome, uf
+having count(1)>1
+
+select nome, uf
+from Cidade
+Group by nome, uf
+having count(1) > 1
+
+commit
+
+
+--15)Adicione uma regra que impeça exista mais de uma cidade com o mesmo nome em um estado.
+
+alter table Cidade
+Add constraint UK_Cidade_NomeUF
+	unique (Nome, UF)
