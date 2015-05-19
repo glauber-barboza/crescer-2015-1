ad5cd408ec3dbbda893fe3d2d69173b33038bf79
Glauber cardoso barboza <glauber.barboza@hotmail.com>
2015-05-19 13:49:49 -0300

    Ajuste js informado pelo bernardo

--- Html/exerciciosAdicionais/donzela/buscadorAjax.js
+++ Html/exerciciosAdicionais/donzela/buscadorAjax.js
@@ -9,9 +9,9 @@ function obterItensComAjaxlimpezaprofunda() {
 		url: 'https://api.spotify.com/v1/artists/6mdiAmATAx73kdxrNrnlao/albums?limit=50',
 		dataType: 'json'
 	}).done(function(data){
-		
+		var alb = $('.albums');
 		$.each(data.items, function(i, itemRecebido){
-		$('.albums').append($('<img>', {src: itemRecebido.images[1].url }));
+		alb.append($('<img>', {src: itemRecebido.images[1].url }));
 	
 		});
 	});
